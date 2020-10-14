265407a03a5c37e7bc865a044eac7cc31854d5e2
towhiid <towhid3008@gmail.com>
2020-10-14 23:49:12 +0600

    client site

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAfuvz_EGOXedOWHV6Yi_0xsvfIvMIhqNI",
+    authDomain: "creative-agency-b50cc.firebaseapp.com",
+    databaseURL: "https://creative-agency-b50cc.firebaseio.com",
+    projectId: "creative-agency-b50cc",
+    storageBucket: "creative-agency-b50cc.appspot.com",
+    messagingSenderId: "920785107811",
+    appId: "1:920785107811:web:75c1af14114e5edf53b156"
+  };
+  export default firebaseConfig;
